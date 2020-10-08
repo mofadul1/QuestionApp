2a99497348b48e04a7d5d8880fe4fcad8fc18c99
mofadul1 <mafkfama@gmail.com>
2020-10-08 09:51:03 -0400

    Initial Commit

---
+++ QuestionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionApp
+//
+//  Created by Mohamed Fadul on 10/8/20.
+//  Copyright © 2020 MoApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
